1c9a3505798df2e0316d30d1275d9c136a7bd5b6
Kevin Ye <kly13@case.edu>
2017-07-03 10:28:53 -0400

    Changes to data validation for survey

--- capstone-project/Capstone.Web/Models/Survey.cs
+++ capstone-project/Capstone.Web/Models/Survey.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.DynamicData;
+using System.ComponentModel.DataAnnotations;
 
 namespace Capstone.Web.Models
 {
     public class Survey
     {
+
         public int SurveyId { get; set; }
+        [Required (ErrorMessage = "Please Select A Park.")]
         public string ParkCode { get; set; }
+        // [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; set; }
+        [Required (ErrorMessage = "A State Is Required.")]
         public string State { get; set; }
         public string ActivityLevel { get; set; }
 
